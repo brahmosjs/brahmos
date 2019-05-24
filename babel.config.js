a4706d598adb378cfda18f01159d071e9947c183
Sudhanshu Yadav <sudhanshuyadav2@gmail.com>
2019-05-24 09:57:35 +0530

    - Moved babel plugin as separate package - Adde repository and bugs link on package.json

--- babel.config.js
+++ babel.config.js
@@ -4,6 +4,6 @@ module.exports = {
     '@babel/plugin-transform-runtime',
     '@babel/plugin-proposal-class-properties',
     ['@babel/plugin-proposal-object-rest-spread', {}, 'rest-spread'],
-    './src/babel/index.js',
+    'brahmos',
   ],
 };
