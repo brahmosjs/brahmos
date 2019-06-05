d2c89a5445eb3dbe61ee32c1762b091a0d2893a2
Sudhanshu Yadav <sudhanshuyadav2@gmail.com>
2019-06-05 11:58:16 +0530

    - Impelement useRef and useReducer hook. - Bail out if lastState is same as newState in both useRef and useReducer hook.

--- src/hooks.js
+++ src/hooks.js
@@ -8,6 +8,10 @@ export function setCurrentComponent (component) {
   component.hooks = component.hooks || [];
 }
 
+/**
+ * A base method to return hook at specific pointer,
+ * and if not available create a new pne
+ */
 function getHook (createHook) {
   const { pointer, hooks } = currentComponent;
   let hook = hooks[pointer];
@@ -23,6 +27,19 @@ function getHook (createHook) {
   return hook;
 }
 
+/**
+ * Function to update component if state is changed
+ */
+function updateComponentIfRequired (component, state, lastState) {
+  if (!Object.is(state, lastState)) {
+    const { __part: part, __componentNode: node } = component;
+    updater([part], [node], [], true);
+  }
+}
+
+/**
+ * Use state hook
+ */
 export function useState (initialState) {
   const component = currentComponent;
   return getHook(() => {
@@ -30,9 +47,49 @@ export function useState (initialState) {
      * create a state hook
      */
     const hook = [initialState, (state) => {
+      const lastState = hook[0];
       hook[0] = state;
-      const { __part: part, __componentNode: node } = component;
-      updater([part], [node], [], true);
+      updateComponentIfRequired(component, state, lastState);
+    }];
+
+    return hook;
+  });
+}
+
+/**
+ * Use ref hook
+ */
+export function useRef (initialValue) {
+  return getHook(() => {
+    /**
+     * create a ref hook
+     */
+    return {
+      current: initialValue,
+    };
+  });
+}
+
+/**
+ * Use reducer hook
+ */
+export function useReducer (reducer, initialState, getInitialState) {
+  const component = currentComponent;
+
+  return getHook(() => {
+    /**
+     * If getInitialState method is provided, use that to form correct initial state
+     * Or else use passed initialState
+     */
+    const _initialState = getInitialState ? getInitialState(initialState) : initialState;
+
+    // create a reducer hook
+    const hook = [_initialState, (action) => {
+      const lastState = hook[0];
+      const state = reducer(lastState, action);
+      hook[0] = state;
+
+      updateComponentIfRequired(component, state, lastState);
     }];
 
     return hook;
