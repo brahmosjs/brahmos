d4702fdd5cb6b528597757d371fb0c18ab63f3ba
Prateek Surana <prateeksurana3255@gmail.com>
2019-06-23 17:58:53 +0530

    :sparkles: started integrating tests

---
+++ src/__tests__/BrahmosES6class.test.js
@@ -0,0 +1,176 @@
+"use strict";
+
+import Brahmos, { render } from "..";
+
+describe("BrahmosES6Class", () => {
+  let container;
+  let Inner;
+  let attachedListener = null;
+  let renderedName = null;
+  beforeEach(() => {
+    attachedListener = null;
+    renderedName = null;
+    container = document.createElement("div");
+    Inner = class extends Brahmos.Component {
+      getName() {
+        return this.props.name;
+      }
+      render() {
+        attachedListener = this.props.onClick;
+        renderedName = this.props.name;
+        return <div className={this.props.name} />;
+      }
+    };
+  });
+
+  function test(element, expectedTag, expectedClassName) {
+    const instance = render(element, container);
+    expect(container.firstChild).not.toBeNull();
+    expect(container.firstChild.tagName).toBe(expectedTag);
+    expect(container.firstChild.className).toBe(expectedClassName);
+    return instance;
+  }
+
+  it("preserves the name of the class for use in error messages", () => {
+    class Foo extends Brahmos.Component {}
+    expect(Foo.name).toBe("Foo");
+  });
+
+  it("renders a simple stateless component with prop", () => {
+    class Foo extends Brahmos.Component {
+      render() {
+        return <Inner name={this.props.bar} />;
+      }
+    }
+    test(<Foo bar="foo" />, "DIV", "foo");
+    test(<Foo bar="bar" />, "DIV", "bar");
+  });
+
+  it('renders based on state using initial values in this.props', () => {
+    class Foo extends Brahmos.Component {
+      constructor(props) {
+        super(props);
+        this.state = {bar: this.props.initialValue};
+      }
+      render() {
+        return <span className={this.state.bar} />;
+      }
+    }
+    test(<Foo initialValue="foo" />, 'SPAN', 'foo');
+  });
+
+  it('renders based on state using props in the constructor', () => {
+    class Foo extends Brahmos.Component {
+      constructor(props) {
+        super(props);
+        this.state = {bar: props.initialValue};
+      }
+      changeState() {
+        this.setState({bar: 'bar'});
+      }
+      render() {
+        if (this.state.bar === 'foo') {
+          return <div className="foo" />;
+        }
+        return <span className={this.state.bar} />;
+      }
+    }
+    const instance = test(<Foo initialValue="foo" />, 'DIV', 'foo');
+    console.log(instance)  // <-- returns undefined should return the component instance instead
+    // instance.changeState()
+    // test(<Foo />, 'SPAN', 'bar');
+  });
+
+  it('sets initial state with value returned by static getDerivedStateFromProps', () => {
+    class Foo extends Brahmos.Component {
+      state = {};
+      static getDerivedStateFromProps(nextProps, prevState) {
+        return {
+          foo: nextProps.foo,
+          bar: 'bar',
+        };
+      }
+      render() {
+        return <div className={`${this.state.foo} ${this.state.bar}`} />;
+      }
+    }
+    test(<Foo foo="foo" />, 'DIV', 'foo bar');
+  });
+
+  it('updates initial state with values returned by static getDerivedStateFromProps', () => {
+    class Foo extends Brahmos.Component {
+      state = {
+        foo: 'foo',
+        bar: 'bar',
+      };
+      static getDerivedStateFromProps(nextProps, prevState) {
+        return {
+          foo: `not-${prevState.foo}`,
+        };
+      }
+      render() {
+        return <div className={`${this.state.foo} ${this.state.bar}`} />;
+      }
+    }
+    test(<Foo />, 'DIV', 'not-foo bar');
+  });
+
+  it('renders updated state with values returned by static getDerivedStateFromProps', () => {
+    class Foo extends Brahmos.Component {
+      state = {
+        value: 'initial',
+      };
+      static getDerivedStateFromProps(nextProps, prevState) {
+        if (nextProps.update) {
+          return {
+            value: 'updated',
+          };
+        }
+        return null;
+      }
+      render() {
+        return <div className={this.state.value} />;
+      }
+    }
+    test(<Foo update={false} />, 'DIV', 'initial');
+    test(<Foo update={true} />, 'DIV', 'updated');
+  });
+
+  it('should render with null in the initial state property', () => {
+    class Foo extends Brahmos.Component {
+      constructor() {
+        super();
+        this.state = null;
+      }
+      render() {
+        return <span />;
+      }
+    }
+    test(<Foo />, 'SPAN', '');
+  });
+
+  it('setState through an event handler', () => {
+    class Foo extends Brahmos.Component {
+      constructor(props) {
+        super(props);
+        this.state = {bar: props.initialValue};
+      }
+      handleClick() {
+        this.setState({bar: 'bar'});
+      }
+      render() {
+        return (
+          <Inner name={this.state.bar} onClick={this.handleClick.bind(this)} />
+        );
+      }
+    }
+    test(<Foo initialValue="foo" />, 'DIV', 'foo');
+    attachedListener();
+    //expect(renderedName).toBe('bar');  <-- The state is getting updated and the component is getting re-rendered but the name is still foo
+                                       // I guess that's because expect is being called before the component is being re-rendered, I don't
+                                       // understand how this test is working in React
+  });
+
+
+
+});
