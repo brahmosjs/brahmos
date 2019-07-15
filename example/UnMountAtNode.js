634b0b234a061210b2baef36d7be8b997b7c8d0e
Revanth Mahesh <rmahesh@reflektive.com>
2019-07-15 09:27:03 +0530

    Adds unmountComponentAtNode top level api.

---
+++ example/UnMountAtNode.js
@@ -0,0 +1,23 @@
+import Brahmos, { Component, unmountComponentAtNode } from '../src';
+
+export default class UnMountAtNode extends Component {
+
+  removeNode = () => {
+    setTimeout(() => {
+    unmountComponentAtNode( document.getElementById( 'unmount-node' ) );
+    }, 1000);
+  }
+  componentWillUnmount(){
+    console.log( 'component will unmount life cycle called!!' );
+  }
+  render () {
+    return (
+     <div>
+      <p> Remove a mounted Brahmos component from the DOM and clean up its event handlers and state. If no component was mounted in the container, calling this function does nothing. Returns true if a component was unmounted and false if there was no component to unmount. </p>
+      <div>
+        <button onClick={ this.removeNode }> Remove node</button>
+      </div>
+     </div>
+    );
+  }
+}
--- example/index.js
+++ example/index.js
@@ -1,4 +1,7 @@
 import App from './App.js';
+import UnMountAtNode from './UnMountAtNode';
 import Brahmos, { render } from '../src';
 
 render(<App />, document.getElementById('app'));
+
+render( <UnMountAtNode/>, document.getElementById('unmount-node'))
