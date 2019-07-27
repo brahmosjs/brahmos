03c631699c63c683b689a85c62763f40fe831100
Sudhanshu Yadav <sudhanshuyadav2@gmail.com>
2019-07-27 14:09:02 +0530

    Eslint fixes

--- src/unmountComponentAtNode.js
+++ src/unmountComponentAtNode.js
@@ -1,11 +1,11 @@
 import tearDown from './tearDown';
 
-function unmountComponentAtNode( container ){
+function unmountComponentAtNode (container) {
   /**
     * if container has a brahmosNode, it will be tear down.
     */
-  if( container.__brahmosNode ){
-    tearDown( container.__brahmosNode, { parentNode: container } );
+  if (container.__brahmosNode) {
+    tearDown(container.__brahmosNode, { parentNode: container });
     return true;
   }
   return false;
