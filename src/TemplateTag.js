2d9f0bba3ccb81653f1d798ce134cd78a33557aa
Sudhanshu Yadav <sudhanshuyadav2@gmail.com>
2020-10-10 18:22:53 +0530

    - Alpha release Brahmos - Add comments on the logic

--- src/TemplateTag.js
+++ src/TemplateTag.js
@@ -1,12 +1,28 @@
 // @flow
 import type { PartMeta, TemplateTagType } from './flow.types';
 
+/**
+ * Parts meta code looks like this.
+ * typeCode|primaryIndex|secondaryIndex
+ *
+ * typeCode = 0 -> its an attribute part
+ * typeCode = 1 | 2 -> Its and node part
+ * typeCode = 2 -> Its has a previous sibling which is an expression
+ *
+ * For attributes
+ * primaryIndex -> Index of element in a flattened element list, for which attribute belongs to.
+ * secondaryIndex -> Dynamic attribute index. This is required to identify which is overriding which.
+ *
+ * For node
+ * primaryIndex -> Index of parent element in a flattened element list.
+ * secondaryIndex -> Index of previous children in the childNodes list of parent.
+ */
 function decodePartMeta(partMetaCode: string): Array<PartMeta> {
   const parts = partMetaCode.split(',');
   return parts.map((partCodeStr) => {
-    const [flagCode, primaryIndex, secondaryIndex] = partCodeStr.split('|');
-    const isAttribute = flagCode === '0';
-    const hasExpressionSibling = flagCode === '2';
+    const [typeCode, primaryIndex, secondaryIndex] = partCodeStr.split('|');
+    const isAttribute = typeCode === '0';
+    const hasExpressionSibling = typeCode === '2';
 
     return {
       isAttribute,
